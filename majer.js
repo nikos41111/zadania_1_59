bc564c7bbb6b9b587ad7b96437b6ecb0b29419b3
nikos41111 <nikodem2175@gmail.com>
2026-06-14 12:28:54 +0200

    Add files via upload

---
+++ majer.js
@@ -0,0 +1,5 @@
+document.write("Mój numer w dzienniku to 11<br><br>");
+document.write("Pierwszy wiersz to:&nbsp;&nbsp;6&nbsp;&nbsp;Nie&nbsp;&nbsp;Tak&nbsp;&nbsp;6<br>");
+document.write("Drugi wiersz to:&nbsp;&nbsp;&nbsp;5&nbsp;&nbsp;Tak&nbsp;&nbsp;Tak&nbsp;&nbsp;7<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("red").bold().strike().fontsize(7)+"<br>"); 
+document.write(document.lastModified.fontcolor("maroon")); 
